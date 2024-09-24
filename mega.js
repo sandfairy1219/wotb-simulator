b247f736055ab6e2cbbac7f6323f2e278983e49a
sandfairy1219 <141256422+sandfairy1219@users.noreply.github.com>
2024-09-24 19:42:05 +0900

    Update mega.js

--- mega.js
+++ mega.js
@@ -443,17 +443,17 @@ log.addEventListener('click', function() {
 let box = document.getElementById('box');
 box.addEventListener('change', function() {
     if (box.value == '1' || box.value == '2') {
-        window.location.href = '/blackbox';
+        window.location.href = '/blackbox.html';
     } else if (box.value == '3') {
-        window.location.href = '/mysterybox';
+        window.location.href = '/mysterybox.html';
     } else if (box.value == '4') {
-        window.location.href = '/cta';
+        window.location.href = '/cta.html';
     } else if (box.value == '5') {
-        window.location.href = '/mega';
+        window.location.href = '/mega.html';
     } else if (box.value == '6') {
-        window.location.href = '/awc'
+        window.location.href = '/awc.html'
     } else if (box.value == '7') {
-        window.location.href = '/massive';
+        window.location.href = '/massive.html';
     } else if (box.value == '8') {
         window.location.href = '/country.html';
     }

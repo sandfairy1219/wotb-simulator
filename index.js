dd8490707b2220db942ebfd588a7627c5507192a
sandfairy1219 <141256422+sandfairy1219@users.noreply.github.com>
2025-06-12 12:23:45 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    mainWindow.loadFile(path.join(__dirname, 'renderer', 'index.html'));
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
---
+++ preload.js
@@ -0,0 +1,4 @@
+// filepath: c:\Users\user\Desktop\wotb-simulator-main\src\preload.js
+window.electronAPI = {
+  // Define any functions or variables you want to expose to the renderer process here
+};
